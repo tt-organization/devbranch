27c96facb4aa7b6421bb93f90953015f4d43b706
jilldianek <jilldianek@gmail.com>
2019-02-27 10:17:36 -0800

    version 1 landing and login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
-<<<<<<< HEAD
 import { HomeScreenComponent } from './home-screen/home-screen.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeScreenComponent },
-=======
-import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
-
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: LandingComponent },
->>>>>>> aabd6ccad6313cb2319575d43b88f15e8d0f4e11
   { path: 'login', component: LoginComponent },
   { path: 'user', component: UserComponent}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
-<<<<<<< HEAD
 import { HomeScreenComponent } from './home-screen/home-screen.component';
-=======
-import { LandingComponent } from './landing/landing.component';
->>>>>>> aabd6ccad6313cb2319575d43b88f15e8d0f4e11
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -17,11 +13,7 @@ import { LandingComponent } from './landing/landing.component';
     AppComponent,
     LoginComponent,
     UserComponent,
-<<<<<<< HEAD
     HomeScreenComponent
-=======
-    LandingComponent
->>>>>>> aabd6ccad6313cb2319575d43b88f15e8d0f4e11
   ],
   imports: [
     BrowserModule,
