8b3d198ed5f58a24e28d9b0fd4d41853f13c04ad
Mike <mdtware@hotmail.com>
2019-03-04 15:37:29 -0800

    Changed login form to formbuilder, new account form working. Needs to be prettier

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  submitted = false;
+  success = false;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.loginForm = this.formBuilder.group ({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
 
   ngOnInit() {
   }
