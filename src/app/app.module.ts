c245dedf914fca2b887c5b4e59c7b59b7ed7042e
Sally Nimmo <sallyhnimmo@gmail.com>
2019-03-11 11:49:24 -0700

    Changed to fix module import issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,30 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { TruckOwnerModule } from './truck-owner/truck-owner.module';
+import { UserModule } from './user/user.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './truck-owner/login/login.component';
 import { UserComponent } from './user/user.component';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
 import { NavComponent } from './nav/nav.component';
-import { NewAccountComponent } from './truck-owner/new-account/new-account.component';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent,
-    LoginComponent,
     UserComponent,
     HomeScreenComponent,
-    NavComponent,
-    NewAccountComponent
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TruckOwnerModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/truck-owner/truck-owner.module.ts
+++ src/app/truck-owner/truck-owner.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
 
 @NgModule({
-  declarations: [LoginComponent, AccountComponent],
+  declarations: [LoginComponent, AccountComponent, NewAccountComponent],
   imports: [
-    CommonModule
+    CommonModule, 
+    ReactiveFormsModule
+  ],
+  exports: [
+    LoginComponent,
+    NewAccountComponent
   ]
 })
 export class TruckOwnerModule { }
