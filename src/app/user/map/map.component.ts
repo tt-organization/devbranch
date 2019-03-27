b5c588482719f988b22ca3c0f7fda0727fe44632
Matt Goodis <goodis9@hotmail.com>
2019-03-27 12:49:35 -0700

    Map working

---
+++ src/app/user/map/map.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MapServiceService } from './map-service.service';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+
+  lat : number;
+  long : number;
+
+  Location : Object;
+
+  constructor(private map: MapServiceService) { }
+
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition(data => {
+      console.log(data);
+      this.lat = data.coords.latitude;
+      this.long = data.coords.longitude;
+    });
+  }
+
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewComponent } from './view/view.component';
+import { MapComponent } from './map/map.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
-  declarations: [ViewComponent],
+  declarations: [ViewComponent, MapComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule,
+    AgmCoreModule
   ]
 })
 export class UserModule { }
