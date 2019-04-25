0fd719f4454efffdbab8dff4eb29db2cfab0a77a
Matt Goodis <39417180+goodis0@users.noreply.github.com>
2019-04-25 11:10:32 -0700

    Update map.component.ts

--- src/app/user/map/map.component.ts
+++ src/app/user/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MapServiceService } from './map-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { TruckerDataService } from './../../data/trucker-data.service';
 
 @Component({
   selector: 'app-map',
@@ -10,11 +12,14 @@ export class MapComponent implements OnInit {
 
     private _lat : number;
     private _long: number;
+    id: number;
 
   Location: Object;
 
-  constructor(private map: MapServiceService) { }
+  constructor(private map: MapServiceService, private route: ActivatedRoute, private data: TruckerDataService) { 
+  this.route.params.subscribe(params => this.id = params.id);
 
+  }
   @Input()
   set lat(lat: number) {
     this._lat = (lat);
@@ -38,10 +43,19 @@ export class MapComponent implements OnInit {
   OnChooseLocation(event) {
     console.log(event);
     if (confirm('Would you like to update your trucks location?')) {
-      // Save it!
+      var request = {
+        Truck_ID: this.id
+      };
       this.lat = event.coords.lat;
-     // this.long = event.coords.lng;
-      //call php script to update database
+      this.long = event.coords.lng;
+      var body = {
+        "Truck_ID" : this.id,
+        "latitude" : this.lat,
+        "longitude" : this.long
+      };
+      this.data.sendLocation(body).subscribe(data => {
+        console.log(data);
+      });
     } else {
       // Do nothing!
     }
